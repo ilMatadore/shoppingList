00762815469b6cde0771353a0eb193c945b9cf2a
ilMatadore <41342409+ilMatadore@users.noreply.github.com>
2018-09-24 13:06:09 -0500

    Add files via upload
    adding files

---
+++ script.js
@@ -0,0 +1,58 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+
+	var done1 = document.createElement("button");
+	done1.appendChild(document.createTextNode("Done!"));
+	li.appendChild(done1);
+	done1.onclick = done;
+
+	var remove = document.createElement("button");
+	remove.appendChild(document.createTextNode("Delete"));
+	li.appendChild(remove);
+
+	remove.onclick = removeIt;
+	
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function done(event) { 
+    event.target.parentNode.classList.toggle("done");
+}
+
+function removeIt(evt) {
+	evt.target.parentNode.remove();
+}
+
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+
+
+
+
